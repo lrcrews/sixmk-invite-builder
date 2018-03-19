e63107014c29e2cfcba7626fbef5f022f9f9c907
ryan <lrcrews@gmail.com>
2018-03-18 21:49:42 -0700

    add in note on line class as the order of points matters for the drawing logic

--- src/app/models/line.ts
+++ src/app/models/line.ts
@@ -1,3 +1,9 @@
+// NOTE: when creating Lines you should start from the left-most point,
+//       and if both points start at the same X then you should start
+//       from the top-most point.
+//
+//       (If both points also have the same Y you're doing lines wrong).
+
 import { Point } from "./point";
 
 export class Line {
